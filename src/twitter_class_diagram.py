b792a66dd5034ec9d7ff55d0c430d006a25746da
Amirmohammad Zarif <Developerboys@gmail.com>
2023-06-24 02:49:30 +0330

    Added descriptive use-cases

--- src/twitter_class_diagram.py
+++ src/twitter_class_diagram.py
@@ -6,30 +6,16 @@ dot.graph_attr['rankdir'] = 'LR'
 with dot.subgraph(name='cluster_TwitterApp') as c:
     c.attr(label='TwitterApp', style='rounded')
     c.attr(bgcolor='lightgray', color='black', fontcolor='black')
-    c.node_attr.update(shape='record')
+    c.node_attr.update(shape='rectangle', height='0', width='0')
     c.edge_attr.update(arrowhead='open')
 
-    c.node('Twitter', r'{class Twitter | -tweets: List<Tweet> | -users: List<User> | '
-                      r'+addTweet(tweet: Tweet): void | +deleteTweet(tweet: Tweet): void | '
-                      r'+searchTweets(keyword: String): List<Tweet> | +followUser(user: User): void | '
-                      r'+unfollowUser(user: User): void}')
-    c.node('User', r'{class User | -username: String | -password: String | -email: String | '
-                   r'-tweets: List<Tweet> | -followers: List<User> | -following: List<User> | '
-                   r'+editProfile(username: String, password: String, email: String): void | '
-                   r'+createTweet(tweet: Tweet): void | +deleteTweet(tweet: Tweet): void | '
-                   r'+getTweets(): List<Tweet> | +getFollowers(): List<User> | +getFollowing(): List<User>}')
-    c.node('Tweet', r'{class Tweet | -id: int | -text: String | -date: Date | -user: User | '
-                    r'+getText(): String | +getDate(): Date | +getUser(): User}')
-    c.node('UserService', r'{class UserService | +registerUser(username: String, password: String, email: String): User | '
-                          r'+loginUser(username: String, password: String): boolean | +logoutUser(): void | '
-                          r'+getUserById(id: int): User | +getUserByUsername(username: String): User}')
-    c.node('TweetService', r'{class TweetService | +createTweet(text: String, user: User): Tweet | '
-                           r'+deleteTweet(tweet: Tweet): void | +getTweetById(id: int): Tweet | '
-                           r'+searchTweets(keyword: String): List<Tweet>}')
-    c.node('FollowService', r'{class FollowService | +followUser(follower: User, following: User): void | '
-                            r'+unfollowUser(follower: User, following: User): void | '
-                            r'+getFollowers(user: User): List<User> | +getFollowing(user: User): List<User>}')
-    c.node('NotificationService', r'{class NotificationService | +sendNotification(user: User, message: String): void}')
+    c.node('Twitter', r'class Twitter\l-tweets: List<Tweet>\l-users: List<User>\l+addTweet(tweet: Tweet): void\l+deleteTweet(tweet: Tweet): void\l+searchTweets(keyword: String): List<Tweet>\l+followUser(user: User): void\l+unfollowUser(user: User): void')
+    c.node('User', r'class User\l-username: String\l-password: String\l-email: String\l-tweets: List<Tweet>\l-followers: List<User>\l-following: List<User>\l+editProfile(username: String, password: String, email: String): void\l+createTweet(tweet: Tweet): void\l+deleteTweet(tweet: Tweet): void\l+getTweets(): List<Tweet>\l+getFollowers(): List<User>\l+getFollowing(): List<User>')
+    c.node('Tweet', r'class Tweet\l-id: int\l-text: String\l-date: Date\l-user: User\l+getText(): String\l+getDate(): Date\l+getUser(): User')
+    c.node('UserService', r'class UserService\l+registerUser(username: String, password: String, email: String): User\l+loginUser(username: String, password: String): boolean\l+logoutUser(): void\l+getUserById(id: int): User\l+getUserByUsername(username: String): User')
+    c.node('TweetService', r'class TweetService\l+createTweet(text: String, user: User): Tweet\l+deleteTweet(tweet: Tweet): void\l+getTweetById(id: int): Tweet\l+searchTweets(keyword: String): List<Tweet>')
+    c.node('FollowService', r'class FollowService\l+followUser(follower: User, following: User): void\l+unfollowUser(follower: User, following: User): void\l+getFollowers(user: User): List<User>\l+getFollowing(user: User): List<User>')
+    c.node('NotificationService', r'class NotificationService\l+sendNotification(user: User, message: String): void')
 
     c.edge('Twitter', 'User')
     c.edge('User', 'Tweet')
